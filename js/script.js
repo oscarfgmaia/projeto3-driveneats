a4548197cbcb42fde08382bc6ac08416db4be2e4
OscarMaia <oscarfgmaia@gmail.com>
2022-08-13 10:19:15 -0300

    Pre-Order finalizada podendo cancelar

--- js/script.js
+++ js/script.js
@@ -5,6 +5,9 @@ let order = document.querySelector('.order');
 let plateOrder;
 let drinkOrder;
 let dessertOrder;
+let platePrice;
+let drinkPrice;
+let dessertPrice;
 let totalValue;
 
 function getPlates() {
@@ -111,27 +114,63 @@ function canOrder(){
 function getOrder(element){
     for(i=0;i<plates.length;i++){
         if(plates[i].classList.contains('selecionado')){
-            plateOrder = plates[i].children.innerHTML;
+            plateOrder =plates[i].getElementsByTagName('div')[1].innerHTML;
+            platePrice =plates[i].getElementsByTagName('div')[3].querySelector('.price').innerHTML;
         }
     }
 
     for(i=0;i<drinks.length;i++){
         if(drinks[i].classList.contains('selecionado')){
-            drinkOrder = drinks[i].innerHTML;
+            drinkOrder = drinks[i].getElementsByTagName('div')[1].innerHTML;
+            drinkPrice =drinks[i].getElementsByTagName('div')[3].querySelector('.price').innerHTML;
         }
     }
 
     for(i=0;i<desserts.length;i++){
         if(desserts[i].classList.contains('selecionado')){
-            dessertOrder = desserts[i].innerHTML;
+            dessertOrder = desserts[i].getElementsByTagName('div')[1].innerHTML;
+            dessertPrice =desserts[i].getElementsByTagName('div')[3].querySelector('.price').innerHTML;            
         }
     }
+}
+
+function refreshSummary(){
+    let summaryItem = document.querySelector('.item1');
+    summaryItem.getElementsByTagName('span')[0].innerHTML = plateOrder;
+    summaryItem.getElementsByTagName('span')[1].innerHTML = platePrice;
+
+    summaryItem = document.querySelector('.item2');
+    summaryItem.getElementsByTagName('span')[0].innerHTML = drinkOrder;
+    summaryItem.getElementsByTagName('span')[1].innerHTML = drinkPrice;
+
+    summaryItem = document.querySelector('.item3');
+    summaryItem.getElementsByTagName('span')[0].innerHTML = dessertOrder;
+    summaryItem.getElementsByTagName('span')[1].innerHTML = dessertPrice;
+    
+
+    platePrice = Number(platePrice.replaceAll(',','.'));
+    drinkPrice = Number(drinkPrice.replaceAll(',','.'));
+    dessertPrice = Number(dessertPrice.replaceAll(',','.'));
+
+    let summaryValueTotal = document.querySelector('.valor-total');
+    totalValue = platePrice+drinkPrice+dessertPrice;
+    let totalValueStr = totalValue.toFixed(2).toString()
+
+    summaryValueTotal.innerHTML = totalValueStr.replaceAll('.',',');
+
 }
 
 function makeOrder(){
     if (order.classList.contains('can-order')){
         let navigate = document.querySelector('.hidden')
         navigate.classList.remove('hidden');
+        getOrder();
+        refreshSummary();
 
     }
+}
+
+function backToMainScreen(){
+    let temp = document.querySelector('.summary');
+    temp.classList.add('hidden');
 }
